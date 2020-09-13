bfc09ee13454f273c7b2c4b81ae35a996f5a466b
Federico Izzo <federico.izzo99@gmail.com>
2020-09-13 13:25:36 +0200

    fix tlp and pulseaudio config

--- laptop/services.nix
+++ laptop/services.nix
@@ -29,14 +29,22 @@
   services.tlp = {
     enable = true;
     # Settings https://linrunner.de/tlp
-    settings = {
-      "TLP_ENABLE" = 1;
-      "TLP_DEFAULT_MODE" = "AC";
-      "WIFI_PWR_ON_AC" = "off";
-      "WIFI_PWR_ON_BAT" = "on";
-      "CPU_HWP_ON_AC" = "performance";
-      "CPU_HWP_ON_BAT" = "balance-performance";
-    };
+    # settings = {
+    #   "TLP_ENABLE" = 1;
+    #   "TLP_DEFAULT_MODE" = "AC";
+    #   "WIFI_PWR_ON_AC" = "off";
+    #   "WIFI_PWR_ON_BAT" = "on";
+    #   "CPU_HWP_ON_AC" = "performance";
+    #   "CPU_HWP_ON_BAT" = "balance-performance";
+    # };
+    settings = "
+      TLP_ENABLE = 1\n
+      TLP_DEFAULT_MODE = AC\n
+      WIFI_PWR_ON_AC = off\n
+      WIFI_PWR_ON_BAT = on\n
+      CPU_HWP_ON_AC = performance\n
+      CPU_HWP_ON_BAT = balance-performance\n
+    ";
   };
 
   services.thermald = {
