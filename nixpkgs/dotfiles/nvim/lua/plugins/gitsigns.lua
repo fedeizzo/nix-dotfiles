5ac3c2e3a5329ff37cd3054fabe5a8048f3eb2bf
Federico Izzo <federico.izzo99@gmail.com>
2021-05-15 15:39:07 +0200

    Add gitsigns setup

---
+++ nixpkgs/dotfiles/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+require('gitsigns').setup {
+    numhl = true,
+    current_line_blame = true,
+    signs = {
+        add          = {hl = 'GitSignsAdd'   , text = '', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+        change       = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+        delete       = {hl = 'GitSignsDelete', text = '', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        topdelete    = {hl = 'GitSignsDelete', text = '', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+        changedelete = {hl = 'GitSignsChange', text = '', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    },
+    keymaps = {
+        noremap = true,
+        buffer = true,
+
+        ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"},
+        ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
+    }
+}
