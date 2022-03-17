a43b678ccbe93cdd03e438b738bc7f42d28c7950
Federico Izzo <federico.izzo99@gmail.com>
2022-03-17 09:28:47 +0100

    remove custom derivation for rbw

--- nixpkgs/laptop/config.nix
+++ nixpkgs/laptop/config.nix
@@ -6,7 +6,7 @@
   joypixels.acceptLicense = true;
   packageOverrides = pkgs: with pkgs; rec {
     swayhide = callPackage ../pkgs/swayhide.nix { };
-    rbw = callPackage ../pkgs/rbw.nix { };
+    # rbw = callPackage ../pkgs/rbw.nix { };
     swaync = callPackage ../pkgs/swaync.nix { };
   };
   permittedInsecurePackages = [
