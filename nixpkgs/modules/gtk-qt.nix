7fbd8049a684ea92ae9afe98194e3648252fd431
Federico Izzo <federico.izzo99@gmail.com>
2022-05-26 21:52:33 +0200

    add support&scripts for the wacom tablet

---
+++ nixpkgs/modules/gtk-qt.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, libs, ... }:
+
+{
+  gtk = {
+    enable = true;
+    font.name = "JetBrains Mono";
+    font.package = pkgs.jetbrains-mono;
+    theme.name = "Nordic";
+    theme.package = pkgs.nordic;
+    # iconTheme.name = "Nordic";
+    # iconTheme.package = pkgs.nordic;
+  };
+}
