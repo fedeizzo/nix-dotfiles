171abecc4bde64050b364aff2e008f506dd972c9
Federico Izzo <federico.izzo99@gmail.com>
2022-07-21 10:31:07 +0200

    update raspberry install script

--- new_raspberry_ext4.sh
+++ new_raspberry_ext4.sh
@@ -4,32 +4,19 @@ HOSTNAME="rasp-nixos"
 BOOT_DEV="/dev/sda"
 ROOT_DEV="/dev/sda"
 
-block_before_install=$1
+wipefs -a $BOOT_DEV
 
 # boot partition UEFI
-sgdisk -n 0:0:+512MiB -t 0:ef00 -c 0:boot "$BOOT_DEV"
+parted -a optimal $BOOT_DEV -- mklabel gpt
+parted -a optimal $BOOT_DEV -- mkpart ESP fat32 1MiB 513MiB
+parted -a optimal $BOOT_DEV -- set 1 esp on
+
 # root partition
-sgdisk -n 0:0:0 -t 0:8300 -c 0:nixenc "$ROOT_DEV"
+parted -a optimal $ROOT_DEV -- mkpart primary 513MiB 100%
+boot="$BOOT_DEV"1
+root="$ROOT_DEV"2
 
-if [[ $BOOT_DEV == $ROOT_DEV ]]; then
-    boot="$BOOT_DEV"1
-    root="$BOOT_DEV"2
-else
-    boot="$BOOT_DEV"1
-    root="$ROOT_DEV"2
-fi
-
-# force re-reading the partition table
-sync
-partprobe "$BOOT_DEV"
-partprobe "$ROOT_DEV"
-
-# print results
-sgdisk -p "$BOOT_DEV"
-sgdisk -p "$ROOT_DEV"
-
-# formatting filesystems
-mkfs.vfat -n boot "$boot"
+mkfs.fat -F 32 -n boot -L boot "$boot"
 mkfs.ext4 -L root "$root"
 
 # mount root on mnt
@@ -42,11 +29,13 @@ mount "$boot" /mnt/boot
 
 if [ -z $block_before_install ]; then
     # setup UEFI
+    cwd=$(pwd)
     nix-env -iA nixos.wget nixos.unzip
     cd /mnt/boot && wget 'https://github.com/pftf/RPi4/releases/download/v1.33/RPi4_UEFI_Firmware_v1.33.zip' && unzip RPi4_UEFI_Firmware_v1.33.zip
     rm /mnt/boot/README.md
     rm RPi4_UEFI_Firmware_v1.33.zip
 
+    cd $cwd
     # setup nix flake
     nix-env -iA nixos.nixUnstable
     nix-env -iA nixos.git
