27e5765265b36dfd2483fd6a399ec7cd8592d776
Federico Izzo <federico.izzo99@gmail.com>
2022-07-27 14:18:07 +0200

    update

--- nixos/raspberry/nixos.nix
+++ nixos/raspberry/nixos.nix
@@ -85,7 +85,6 @@
         dmenu #application launcher most people use
         i3status # gives you the default i3 status bar
         i3lock #default i3 screen locker
-        i3blocs #if you are planning on using i3blocks over i3status
       ];
     };
   };
