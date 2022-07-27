05a0018adf9997cc07d9b7a4f59f2e4c26bb82b6
Federico Izzo <federico.izzo99@gmail.com>
2022-07-27 14:00:58 +0200

    add i3 to rasp config

--- nixos/raspberry/nixos.nix
+++ nixos/raspberry/nixos.nix
@@ -50,6 +50,8 @@
     git
     vim
     docker-compose
+    dnsmasq
+    hostapd
   ];
   virtualisation = {
     docker = {
@@ -61,6 +63,29 @@
   programs.bash = {
     enableCompletion = true;
     enableLsColors = true;
+  };
+
+  # i3
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocs #if you are planning on using i3blocks over i3status
+      ];
+    };
   };
 
   # SECURITY
