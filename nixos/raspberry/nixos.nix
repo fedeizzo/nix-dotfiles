77207b6084e6357662a6c93dc34f108b6fb3c683
Federico Izzo <federico.izzo99@gmail.com>
2022-08-05 20:15:11 +0200

    update nixos config

--- nixos/raspberry/nixos.nix
+++ nixos/raspberry/nixos.nix
@@ -26,148 +26,178 @@
 
   # NETWORKING
   networking = {
-    hostName = "rasp-nixos"; # Define your hostname.
+    hostName = "rasp-nixos";
     networkmanager = {
       enable = true;
     };
-    # defaultGateway = "192.168.119.255";
-    # nameservers = [ "8.8.8.8" ];
-    # interfaces.eth0.ipv4.addresses = [{
-    #   address = "192.168.118.0";
-    #   prefixLength = 22;
-    # }];
-  };
-  # services.hostapd = {
-  #   enable = true;
-  #   ssid = "Rete1220J";
-  #   interface = "wlp1s0u1u3";
-  #   # wpaPassphrase = "";
-  # };
-  # services.dnsmasq = {
-  #   enable = true;
-  #   servers = [
-  #     "8.8.8.8"
-  #     "8.8.4.4"
-  #   ];
-  #   extraConfig = ''
-  #     interface=wlp1s0u1u3
-  #     listen-address = 192.168.5.1
-  #     bind-interfaces
-  #     domain-needed
-  #     bogus-priv
-  #     dhcp-range=192.168.5.50,192.168.5.150,12h
-  #   '';
-  # };
-  # boot.kernel.sysctl = {
-  #   net.ipv4.ip_forward = 1;
-  # };
-  networking.firewall.enable = true;
-  networking.firewall.trustedInterfaces = [ "tailscale0" ];
-
-  # SSH & VPN
-  services.tailscale.enable = true;
-  services.openssh.enable = true;
-
-
-  # KEYMAP AND TIME
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    keyMap = "us";
-  };
-  time.timeZone = "Europe/Rome";
-  time.hardwareClockInLocalTime = true;
-
-  # PACKAGES
-  environment.systemPackages = with pkgs; [
-    raspberrypifw
-    bc
-    curl
-    killall
-    wget
-    git
-    vim
-    docker-compose
-    dnsmasq
-    hostapd
-    firefox
-  ];
-  virtualisation = {
-    docker = {
+    interfaces.eth0.ipv4.addresses = [{
+      address = "192.168.1.31";
+      prefixLength = 24;
+    }];
+    nat = {
       enable = true;
-      enableOnBoot = true;
-      enableNvidia = false;
+      externalInterfaces = "wlp1s0u1u3";
+      internalInterfaces = [ "wg0" ];
     };
-  };
-  programs.bash = {
-    enableCompletion = true;
-    enableLsColors = true;
-  };
-
-  # i3
-  services.xserver = {
-    enable = true;
-
-    desktopManager = {
-      xterm.enable = false;
-    };
-
-    displayManager = {
-      startx.enable = false;
-      defaultSession = "none+i3";
-    };
-
-    windowManager.i3 = {
+    firewall = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
+      trustedInterfaces = [ "wg0" ];
+      allowedUDPPorts = [ 51820 ];
+    };
+    wireguard = {
+      enable = true;
+      interfaces = {
+        wg0 = {
+          ips = [ "192.168.1.31" ];
+
+          listenPort = 51820;
+
+          postSetup = ''
+            ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+          '';
+
+          postShutdown = ''
+            ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+          '';
+
+          # Path to the private key file.
+          #
+          # Note: The private key can also be included inline via the privateKey option,
+          # but this makes the private key world-readable; thus, using privateKeyFile is
+          # recommended.
+          # privateKeyFile = "path to private key file";
+          privateKey = "TODO";
+
+          peers = [
+            # {
+            #   # John Doe
+            #   publicKey = "{john doe's public key}";
+            #   # visible other machine in the private network
+            #   allowedIPs = [ "10.100.0.3/32" ];
+            # }
+          ];
+        };
+      };
+    };
+
+    services.openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      allowSFTP = false; # Don't set this if you need sftp
+      challengeResponseAuthentication = false;
+      passwordAuthentication = false;
+      openFirewall = false;
+      forxardX11 = false;
+      permitRootLogin = "no";
+    };
+    services.fail2ban.enable = true;
+
+    # KEYMAP AND TIME
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+      keyMap = "us";
+    };
+    time.timeZone = "Europe/Rome";
+    time.hardwareClockInLocalTime = true;
+
+    # PACKAGES
+    environment.systemPackages = with pkgs; [
+      raspberrypifw
+      bc
+      curl
+      killall
+      wget
+      git
+      vim
+      docker-compose
+      dnsmasq
+      hostapd
+      firefox
+    ];
+    virtualisation = {
+      docker = {
+        enable = true;
+        enableOnBoot = true;
+        enableNvidia = false;
+      };
+    };
+    programs.bash = {
+      enableCompletion = true;
+      enableLsColors = true;
+    };
+
+    # i3
+    # services.xserver = {
+    #   enable = true;
+
+    #   desktopManager = {
+    #     xterm.enable = false;
+    #   };
+
+    #   displayManager = {
+    #     startx.enable = false;
+    #     defaultSession = "none+i3";
+    #   };
+
+    #   windowManager.i3 = {
+    #     enable = true;
+    #     extraPackages = with pkgs; [
+    #       dmenu #application launcher most people use
+    #       i3status # gives you the default i3 status bar
+    #       i3lock #default i3 screen locker
+    #     ];
+    #   };
+    # };
+
+    # SECURITY
+    security.sudo.enable = false;
+    security.doas = {
+      enable = true;
+      extraRules = [
+        { groups = [ "wheel" ]; keepEnv = true; persist = true; }
       ];
     };
-  };
-
-  # SECURITY
-  security.doas = {
-    enable = true;
-    extraRules = [
-      { groups = [ "wheel" ]; keepEnv = true; persist = true; }
+    # Show log with journactl -f
+    security.auditd.enable = true;
+    security.audit.enable = true;
+    security.audit.rules = [
+      "-a exit,always -F arch=b64 -S execve"
     ];
-  };
 
-  # USER
-  users.users.rasp = {
-    name = "rasp";
-    isNormalUser = true;
-    createHome = true;
-    extraGroups = [
-      "wheel"
-      "docker"
-      "autologin"
-      "users"
-      "networkmanager"
-    ];
-    shell = pkgs.bash;
-  };
-
-  # NIX STUFF
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-  nix = {
-    autoOptimiseStore = true;
-    package = pkgs.nixFlakes;
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
+    # USER
+    users.users.rasp = {
+      name = "rasp";
+      isNormalUser = true;
+      createHome = true;
+      extraGroups = [
+        "wheel"
+        "docker"
+        "autologin"
+        "users"
+        "networkmanager"
+      ];
+      shell = pkgs.bash;
     };
-    # Free up to 1GiB whenever there is less than 100MiB left.
-    extraOptions = ''
-        min-free = ${toString (100 * 1024 * 1024)}
-      max-free = ${toString (1024 * 1024 * 1024)}
-      experimental-features = nix-command flakes
-    '';
-  };
-  system.stateVersion = "22.05";
-}
+
+    # NIX STUFF
+    nixpkgs.config = {
+      allowUnfree = true;
+    };
+    nix = {
+      autoOptimiseStore = true;
+      package = pkgs.nixFlakes;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+      # Free up to 1GiB whenever there is less than 100MiB left.
+      extraOptions = ''
+          min-free = ${toString (100 * 1024 * 1024)}
+        max-free = ${toString (1024 * 1024 * 1024)}
+        experimental-features = nix-command flakes
+      '';
+    };
+    system.stateVersion = "22.05";
+  }
 
