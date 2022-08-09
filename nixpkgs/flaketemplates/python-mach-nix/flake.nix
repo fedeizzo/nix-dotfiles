1f2210dddd4249c6b18a4e234d43c3b36c029453
Federico Izzo <federico.izzo99@gmail.com>
2022-08-09 10:54:44 +0200

    udpate flake template

--- nixpkgs/flaketemplates/python-mach-nix/flake.nix
+++ nixpkgs/flaketemplates/python-mach-nix/flake.nix
@@ -1,30 +1,47 @@
 {
   description = "Desc";
   inputs = {
-    mach-nix.url = "mach-nix/3.5.0";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mach-nix = {
+      url = "github:DavHau/mach-nix/3.5.0";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+    };
   };
 
-  outputs = { self, nixpkgs, mach-nix }:
+  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@attr:
     let
-      system = "x86_64-linux";
-      req = builtins.filter (builtins.isString) (builtins.split "\n" (builtins.readFile ./requirements.txt));
-      isComment = x: ! builtins.isNull (builtins.match "^#.*" x);
-      hasVersion = x: ! builtins.isNull (builtins.match ".*=.*" x);
-      deleteHttps = x: if (builtins.isNull (builtins.match "https.*" x)) then x else "";
-      deleteGit = x: if (builtins.isNull (builtins.match "git.*" x)) then x else "";
-      cleanNonMachNixCompatible = x: (deleteGit (deleteHttps x));
-      removeVersion = x: if ((isComment x) || (! hasVersion x)) then [ (cleanNonMachNixCompatible x) ] else (builtins.match "([^=><~]*).*" x);
-      cleanedReq = builtins.concatStringsSep "\n" (builtins.concatMap removeVersion req);
+      requirements = (builtins.readFile ./requirements.txt);
+      cleanedRequirements = (builtins.replaceStrings [ ] [ ] requirements);
     in
-    {
-      packages."${system}".default = mach-nix.lib."${system}".buildPythonPackage {
-        src = ./.;
-      };
-      devShells."${system}".default = mach-nix.lib."${system}".mkPythonShell {
-        requirements = builtins.readFile ./requirements.txt;
-        packagesExtra = [
-          ./.
-        ];
-      };
-    };
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        mach = mach-nix.lib.${system};
+
+        myPython = mach.mkPython {
+          python = "python39";
+
+          requirements = cleanedRequirements;
+          packagesExtra = [
+            ./.
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            myPython
+          ];
+        };
+        defaultPackage = myPython;
+      }
+    );
 }
