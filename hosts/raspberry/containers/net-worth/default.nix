3fe4cb3350d9384c155ffc2f216ac6bf18c8dc52
Federico Izzo <federico.izzo99@gmail.com>
2023-10-22 14:05:56 +0200

    bug fix

--- hosts/raspberry/containers/net-worth/default.nix
+++ hosts/raspberry/containers/net-worth/default.nix
@@ -8,10 +8,10 @@
     image = "postgres:16.0-alpine";
     autoStart = true;
     extraOptions = [ "--network=homelab" ];
-    # ports = [ "9999:5432" ];
+    ports = [ "9999:5432" ];
     environmentFiles = [ "/var/container_envs/net_worth_db" ];
     volumes = [
-      "/var/volumes/net_worth_db:/var/run/lib/postgresql/data"
+      "/var/volumes/net_worth_db:/var/lib/postgresql/data"
       "/etc/homelab/net_worth_db/init.sql:/docker-entrypoint-initdb.d/init.sql"
     ];
   };
