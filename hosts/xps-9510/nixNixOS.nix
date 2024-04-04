649e582ddbcc7b23ba287a517d366e9874ab774b
Federico Izzo <federico.izzo@datadoghq.com>
2024-04-04 21:01:56 +0200

    change nix version

--- hosts/xps-9510/nixNixOS.nix
+++ hosts/xps-9510/nixNixOS.nix
@@ -19,7 +19,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-    package = nixpkgs-unstable.nixFlakes;
+    package = nixpkgs-unstable.nixVersions.nix_2_19;
     extraOptions = ''
       experimental-features = nix-command flakes
       keep-outputs = true
