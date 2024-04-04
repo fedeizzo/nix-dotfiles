25c5114944b4c9cb8312680e16d11aa07a556ea3
Federico Izzo <federico.izzo@datadoghq.com>
2024-04-04 20:54:30 +0200

    update nix version to avoid insecure package error

--- hosts/xps-9510/nixNixOS.nix
+++ hosts/xps-9510/nixNixOS.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, nixpkgs-unstable, inputs, ... }:
 
 {
   # nixpkgs = {
@@ -19,7 +19,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
-    package = pkgs.nixFlakes;
+    package = nixpkgs-unstable.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
       keep-outputs = true
