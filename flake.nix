e09afecc0fcf0c6243feb1625674a64734bac39c
Federico Izzo <federico.izzo@datadoghq.com>
2024-04-05 17:24:44 +0200

    add remote build for the homelab

--- flake.nix
+++ flake.nix
@@ -143,6 +143,7 @@
           magicRollback = true;
           autoRollback = true;
           fastConnection = false;
+          remoteBuild = true;
           profiles.system = {
             user = "root";
             path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.rasp-nixos;
