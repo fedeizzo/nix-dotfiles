3182f574fa3cc5eaefeba67038bf9081841cb1f0
Federico Izzo <federico.izzo@datadoghq.com>
2024-05-23 21:23:41 +0200

    fix emacs

--- home/common/emacs/modules/prog/packages/sidecar-locals.el
+++ home/common/emacs/modules/prog/packages/sidecar-locals.el
@@ -4,11 +4,12 @@
 ;; Per project configuration
 
 ;;; Code:
-(use-package sidecar-locals
-  :config
-  (setq sidecar-locals-paths-allow (list
-			            (concat (file-symlink-p (getenv "DATADOG_ROOT")) "/")
-			            (concat (getenv "DATADOG_ROOT") "/")))
-  (sidecar-locals-mode))
+(if ON-MACBOOK
+    (use-package sidecar-locals
+      :config
+      (setq sidecar-locals-paths-allow (list
+			                (concat (file-symlink-p (getenv "DATADOG_ROOT")) "/")
+			                (concat (getenv "DATADOG_ROOT") "/")))
+      (sidecar-locals-mode)))
 
 ;;; sidecar-locals.el ends here
