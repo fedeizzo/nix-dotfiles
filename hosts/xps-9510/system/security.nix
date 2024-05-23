1e21688bc82e0c90bfe41e91810bb7d85e49fff8
Federico Izzo <federico.izzo@datadoghq.com>
2024-05-23 20:58:27 +0200

    change hyprlock

--- hosts/xps-9510/system/security.nix
+++ hosts/xps-9510/system/security.nix
@@ -4,6 +4,10 @@
   security.pam.services.login.fprintAuth = true;
   security.pam.services.system-local-login.fprintAuth = true;
   security.pam.services.doas.fprintAuth = true;
+  security.pam.services.hyprlock = {
+    unixAuth = true;
+    fprintAuth = true;
+  };
   security.sudo.enable = false;
   security.doas = {
     enable = true;
