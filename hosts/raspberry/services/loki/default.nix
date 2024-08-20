e256e13e9e5e0bb56ce70341e5fd7e79a348bf71
Federico Izzo <federico.izzo@datadoghq.com>
2024-08-20 17:12:54 +0200

    add loki

--- hosts/raspberry/services/loki/default.nix
+++ hosts/raspberry/services/loki/default.nix
@@ -4,6 +4,44 @@
   services.loki = {
     enable = true;
     dataDir = "/var/volumes/loki";
-    # configuration = { };
+  };
+
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server = {
+        http_listen_port = 28183;
+        grpc_listen_port = 0;
+      };
+
+      positions = {
+        filename = /tmp/positions.yaml;
+      };
+
+      clients = [
+        {
+          url = "http://127.0.0.1:3100/loki/api/v1/push";
+        }
+      ];
+
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "rasp-nixos";
+            };
+          };
+          relabel_configs = [
+            {
+              source_labels = [ "__journal__systemd_unit" ];
+              target_label = "unit";
+            }
+          ];
+        }
+      ];
+    };
   };
 }
