d9fb0b87660a4026a9c6c27c3a6d2bc51c7868c6
Federico Izzo <federico.izzo@datadoghq.com>
2024-08-20 12:02:38 +0200

    update prometheus

--- hosts/raspberry/services/prometheus/default.nix
+++ hosts/raspberry/services/prometheus/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.prometheus = {
@@ -6,8 +6,29 @@
     alertmanagers = [ ];
     exporters = {
       systemd.enable = true;
+      node.enabled = true;
       wireguard.enable = true;
     };
+    scrapeConfigs = [
+      {
+        job_name = "systemd";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.systemd.port}" ];
+        }];
+      }
+      {
+        job_name = "rasp-nixos";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+      {
+        job_name = "wireguard";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.wireguard.port}" ];
+        }];
+      }
+    ];
     stateDir = "prometheus2";
   };
 }
