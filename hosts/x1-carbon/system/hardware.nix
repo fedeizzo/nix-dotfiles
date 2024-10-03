486ad628c8f5d878a4fca59a0cf0138009561cc8
Federico Izzo <federico.izzo@datadoghq.com>
2024-10-03 16:39:18 +0200

    x1 carbon hardware updates

--- hosts/x1-carbon/system/hardware.nix
+++ hosts/x1-carbon/system/hardware.nix
@@ -2,8 +2,25 @@
 
 {
   hardware.cpu.intel.updateMicrocode = true;
+  hardwer.trackpoint = {
+    enable = true;
+    speed = 200;
+    sensitivity = 200;
+  };
+  services.illum.enable = true; # Enable the brightness buttons
   services.pcscd.enable = true;
   services.fwupd.enable = true; # update firmware
+
+  # https://nicholaslyz.com/blog/2024/04/29/how-to-undervolt-a-laptop-with-nixos/
+  services.undervolt = {
+    enable = true;
+    coreOffset = -80;
+    uncoreOffset = -80;
+    gpuOffset = -60;
+    turbo = 0; # enabled
+    p1.limit = 20;
+    p2.limit = 40;
+  };
 
   imports = [
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
--- hosts/x1-carbon/system/keyboardKeymapFont.nix
+++ hosts/x1-carbon/system/keyboardKeymapFont.nix
@@ -5,28 +5,27 @@
     ../../common/system/keyboardKeymapFont.nix
   ];
 
-  # services = {
-  #   keyd = {
-  #     enable = true;
-  #     keyboards = {
-  #       default = {
-  #         ids = [ "0001:0001" ];
-  #         settings = {
-  #           main = {
-  #             capslock = "overload(control, leftcontrol)";
-  #             leftcontrol = "capslock";
-  #           };
-  #         };
-  #       };
-  #     };
-  #     fprintd = {
-  #       enable = true;
-  #       package = pkgs.fprintd;
-  #       tod = {
-  #         enable = true;
-  #         driver = pkgs.libfprint-2-tod1-goodix;
-  #       };
-  #     };
-  #   };
-  # };
+  services = {
+    #   keyd = {
+    #     enable = true;
+    #     keyboards = {
+    #       default = {
+    #         ids = [ "0001:0001" ];
+    #         settings = {
+    #           main = {
+    #             capslock = "overload(control, leftcontrol)";
+    #             leftcontrol = "capslock";
+    #           };
+    #         };
+    #       };
+    #     };
+    fprintd = {
+      enable = true;
+      package = pkgs.fprintd;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-3-tod1-vfs0090;
+      };
+    };
+  };
 }
