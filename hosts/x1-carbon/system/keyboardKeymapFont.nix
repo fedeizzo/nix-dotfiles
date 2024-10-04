6ccf1edc567def36128c8db25e5639f70e310b3c
Federico Izzo <federico.izzo@datadoghq.com>
2024-10-04 22:06:11 +0200

    some hardware fixes

--- hosts/x1-carbon/system/keyboardKeymapFont.nix
+++ hosts/x1-carbon/system/keyboardKeymapFont.nix
@@ -6,25 +6,26 @@
   ];
 
   services = {
-    #   keyd = {
-    #     enable = true;
-    #     keyboards = {
-    #       default = {
-    #         ids = [ "0001:0001" ];
-    #         settings = {
-    #           main = {
-    #             capslock = "overload(control, leftcontrol)";
-    #             leftcontrol = "capslock";
-    #           };
-    #         };
-    #       };
-    #     };
+    keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "0001:0001" ];
+          settings = {
+            main = {
+              capslock = "overload(control, leftcontrol)";
+              leftcontrol = "capslock";
+            };
+          };
+        };
+      };
+    };
     fprintd = {
       enable = true;
       package = pkgs.fprintd;
       tod = {
         enable = true;
-        driver = pkgs.libfprint-2-tod1-vfs0090;
+        driver = pkgs.libfprint-tod;
       };
     };
   };
