e3178e711b66f44103c3b9c10d7d3d07cb12f325
Federico Izzo <federico.izzo99@gmail.com>
2024-10-12 09:58:46 +0200

    refactor: remove eww

--- home/x1-carbon/modules/wayland/kanshi.nix
+++ home/x1-carbon/modules/wayland/kanshi.nix
@@ -15,7 +15,6 @@
             mode = "1920x1200@59.950Hz";
           }
         ];
-        profile.exec = [ "eww --no-daemonize open-many clock sys-info-panel bluetooth-info-panel backup workspaces" ];
       }
       {
         profile.name = "docked";
@@ -30,7 +29,6 @@
             mode = "3440x1440@60.0Hz";
           }
         ];
-        profile.exec = [ "eww --no-daemonize open-many clock sys-info-panel bluetooth-info-panel backup workspaces" ];
       }
       {
         profile.name = "docked2";
@@ -45,7 +43,6 @@
             mode = "3440x1440@60.0Hz";
           }
         ];
-        profile.exec = [ "eww --no-daemonize open-many clock sys-info-panel bluetooth-info-panel backup workspaces" ];
       }
     ];
   };
