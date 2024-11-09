938d6e70ede1ee886bfd9e47edeb2bf2f0275d61
Federico Izzo <federico.izzo99@gmail.com>
2024-11-09 16:45:12 +0100

    refactor(homelab): remove sway references and update sunshine

--- hosts/xps-9510-homelab/services/sunshine/default.nix
+++ hosts/xps-9510-homelab/services/sunshine/default.nix
@@ -1,24 +1,5 @@
-{ pkgs, hostname, config, ... }:
+{ pkgs, hostname, ... }:
 
-let
-  sway-steam-deck-resolution = pkgs.writers.writeBashBin "steam-deck-res" ''
-    ${pkgs.sway}/bin/sway 'output eDP-1 mode --custom 1280x800@60.00Hz'
-    ${pkgs.sway}/bin/sway 'workspace 2'
-  '';
-  sway-full-hd-resolution = pkgs.writers.writeBashBin "full-hd-res" ''
-    ${pkgs.sway}/bin/sway 'output eDP-1 mode --custom 1920x1080@60.00Hz'
-    ${pkgs.sway}/bin/sway 'workspace 2'
-  '';
-  # sway-ultrawide-resolution = pkgs.writers.writeBashBin "ultrawide-res" ''
-  #   ${pkgs.sway}/bin/sway 'output eDP-1 mode --custom 3440x1440@60.00Hz'
-  #   ${pkgs.sway}/bin/sway 'workspace 2'
-  # '';
-  sway-native-resolution = pkgs.writers.writeBashBin "native-res" ''
-    ${pkgs.sway}/bin/sway 'output eDP-1 mode 1920x1200@60.00Hz'
-    ${pkgs.sway}/bin/sway 'workspace 1'
-  '';
-  sunshineSettingsFile = (pkgs.formats.keyValue { }).generate "sunshine.conf" config.services.sunshine.settings;
-in
 {
   users.users.fridge = {
     isNormalUser = true;
@@ -51,43 +32,32 @@ in
       };
 
       applications = {
-        # env = { };
         apps = [
           {
-            name = "Desktop";
-          }
-          {
-            name = "FullHD";
+            name = "Steam Big Picture - steam deck";
             prep-cmd = [
               {
-                do = "${sway-full-hd-resolution}/bin/full-hd-res";
-                undo = "${sway-native-resolution}/bin/native-res";
-              }
-            ];
-          }
-          {
-            name = "Ultrawide";
-            prep-cmd = [
-              {
-                do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.$SUNSHINE_CLIENT_WIDTHx$SUNSHINE_CLIENT_HEIGHT@$SUNSHINE_CLIENT_FPS";
+                do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.1280x800@60";
                 undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.1920x1200@60";
-              }
-            ];
-            exclude-global-prep-cmd = "false";
-            auto-detach = "true";
-          }
-          {
-            name = "Steam Big Picture";
-            prep-cmd = [
-              {
-                do = "${sway-steam-deck-resolution}/bin/steam-deck-res";
-                undo = "${sway-native-resolution}/bin/native-res";
               }
             ];
             detached = [
               "${pkgs.steam}/bin/steam steam://open/bigpicture"
             ];
             image-path = "steam.png";
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
+          }
+          {
+            name = "Desktop";
+            prep-cmd = [
+              {
+                do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.1920x1080@60";
+                undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.eDP-1.mode.1920x1200@60";
+              }
+            ];
+            exclude-global-prep-cmd = "false";
+            auto-detach = "true";
           }
         ];
       };
@@ -117,39 +87,15 @@ in
       enable = true;
       enableRenice = true;
     };
-
-    sway = {
-      enable = true;
-      wrapperFeatures.gtk = true;
-    };
-  };
-
-  # to automatically start sunshine with systemd
-  systemd.user.targets.sway-session = {
-    unitConfig = {
-      Description = "sway compositor session";
-      Documentation = [ "man:systemd.special(7)" ];
-      BindsTo = [ "graphical-session.target" ];
-      Wants = [ "graphical-session-pre.target" ];
-      After = [ "graphical-session-pre.target" ];
-    };
   };
 
   # KDE
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-  # hardware.pulseaudio.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
-  services.getty.autologinUser = "fridge";
   environment = {
     systemPackages = with pkgs; [ protonup-qt firefox ];
-    # in .local/bin/sway there is a custom binary with cap_sys_nice capabilities
-    # in this way each game should re-nice itself to improve the gaming experience
-    # loginShellInit = ''
-    #   [[ "$(tty)" = "/dev/tty1" ]] && sway --unsupported-gpu
-    # '';
-    etc."sunshine/sunshine.conf".source = sunshineSettingsFile;
   };
 }
