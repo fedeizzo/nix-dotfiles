0a0a198955f6a01edb69c390c6599862f587b75b
Federico Izzo <federico.izzo99@gmail.com>
2024-12-01 09:49:35 +0100

    refactor(homelab): move networth db from docker to system

--- hosts/xps-9510-homelab/services/net-worth/default.nix
+++ hosts/xps-9510-homelab/services/net-worth/default.nix
@@ -4,22 +4,22 @@ _:
   ############
   # DATABASE #
   ############
-  virtualisation.oci-containers.containers."net_worth_db" = {
-    image = "postgres:16.0-alpine";
-    autoStart = true;
-    extraOptions = [ "--network=networth" ];
-    ports = [ "9999:5432" ];
-    environmentFiles = [ "/var/container_envs/net_worth_db" ];
-    volumes = [
-      "/var/volumes/net_worth_db:/var/lib/postgresql/data"
-      "/etc/homelab/net_worth_db/init.sql:/docker-entrypoint-initdb.d/init.sql"
-    ];
-  };
-  environment.etc.net_worth_db_init = {
-    enable = true;
-    source = ./init.sql;
-    target = "homelab/net_worth_db/init.sql";
-  };
+  # virtualisation.oci-containers.containers."net_worth_db" = {
+  #   image = "postgres:16.0-alpine";
+  #   autoStart = true;
+  #   extraOptions = [ "--network=networth" ];
+  #   ports = [ "9999:5432" ];
+  #   environmentFiles = [ "/var/container_envs/net_worth_db" ];
+  #   volumes = [
+  #     "/var/volumes/net_worth_db:/var/lib/postgresql/data"
+  #     "/etc/homelab/net_worth_db/init.sql:/docker-entrypoint-initdb.d/init.sql"
+  #   ];
+  # };
+  # environment.etc.net_worth_db_init = {
+  #   enable = true;
+  #   source = ./init.sql;
+  #   target = "homelab/net_worth_db/init.sql";
+  # };
 
   ##############
   # DATA ENTRY #
