e5857d0199dae1a7e288e10925fb51ce2cecf9ee
Federico Izzo <federico@fedeizzo.dev>
2025-01-05 13:06:29 +0100

    feat(homelab): update services dashboard

--- hosts/xps-9510-homelab/services/glance/default.nix
+++ hosts/xps-9510-homelab/services/glance/default.nix
@@ -23,7 +23,7 @@ _:
                 {
                   type = "monitor";
                   cache = "1m";
-                  title = "Services";
+                  title = "Streaming";
                   sites = [
                     { title = "Jellyfin"; url = "http://homelab:8096"; icon = "si:jellyfin"; }
                     { title = "Jellyseerr"; url = "http://homelab:5055"; icon = "si:jellyfin"; }
@@ -32,7 +32,24 @@ _:
                     { title = "Prowlar"; url = "http://homelab:9696"; icon = "si:searxng"; }
                     { title = "Deluge"; url = "http://homelab:8112"; icon = "si:deluge"; }
                     { title = "Bazarr"; url = "http://homelab:6767"; icon = "si:bookstack"; }
+                  ];
+                }
+                {
+                  type = "monitor";
+                  cache = "1m";
+                  title = "Multimedia";
+                  sites = [
                     { title = "Immich"; url = "https://photo.fedeizzo.dev"; icon = "si:immich"; }
+                    { title = "Drive"; url = "https://drive.fedeizzo.dev"; icon = "si:protondrive"; }
+                    { title = "Sunshine"; url = "https://homelab:47990"; icon = "si:stadia"; }
+                  ];
+                }
+                {
+                  type = "monitor";
+                  cache = "1m";
+                  title = "Observability";
+                  sites = [
+                    { title = "Grafana"; url = "https://grafana.fedeizzo.dev"; icon = "si:grafana"; }
                     { title = "Traefik"; url = "http://homelab:8080"; icon = "si:traefikproxy"; }
                   ];
                 }
