272dec96b55ccd805d1997000b0cc3317e593628
Federico Izzo <federico@fedeizzo.dev>
2025-01-27 18:10:07 +0100

    fix: temporary disable wireguard

--- hosts/x1-nano/system/networking.nix
+++ hosts/x1-nano/system/networking.nix
@@ -15,23 +15,23 @@
       allowedUDPPorts = [ 51821 ];
     };
     interfaces.wlp2s0 = { }; # define the interface for the topology network generation
-    wireguard.enable = true;
-    wireguard.interfaces = {
-      wg0 = {
-        ips = [ "192.168.7.3/24" ];
-        listenPort = 51821;
-        privateKeyFile = "${config.sops.secrets.x1-wireguard-private-key.path}";
-        peers = [
-          {
-            # home-lab
-            publicKey = "Ug1P6UzLyQ0BFbrfi0G9KLJBNBs+IOisRn3uiLoR5yU=";
-            allowedIPs = [ "192.168.7.1" ];
-            endpoint = "vpn.fedeizzo.dev:51821";
-            # dynamicEndpointRefreshSeconds = 5;
-            persistentKeepalive = 25;
-          }
-        ];
-      };
-    };
+    # wireguard.enable = true;
+    # wireguard.interfaces = {
+    #   wg0 = {
+    #     ips = [ "192.168.7.3/24" ];
+    #     listenPort = 51821;
+    #     privateKeyFile = "${config.sops.secrets.x1-wireguard-private-key.path}";
+    #     peers = [
+    #       {
+    #         # home-lab
+    #         publicKey = "Ug1P6UzLyQ0BFbrfi0G9KLJBNBs+IOisRn3uiLoR5yU=";
+    #         allowedIPs = [ "192.168.7.1" ];
+    #         endpoint = "vpn.fedeizzo.dev:51821";
+    #         # dynamicEndpointRefreshSeconds = 5;
+    #         persistentKeepalive = 25;
+    #       }
+    #     ];
+    #   };
+    # };
   };
 }
