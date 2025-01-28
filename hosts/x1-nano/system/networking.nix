77712557d3cf787d36ccefc168e5480ec1effb7b
Federico Izzo <federico@fedeizzo.dev>
2025-01-28 19:39:42 +0100

    feat(oven): re-enable wireguard for homelab vpn

--- hosts/x1-nano/system/networking.nix
+++ hosts/x1-nano/system/networking.nix
@@ -14,24 +14,24 @@
       checkReversePath = "loose";
       allowedUDPPorts = [ 51821 ];
     };
-    # interfaces.wlp0s20f3 = { }; # define the interface for the topology network generation
-    # wireguard.enable = true;
-    # wireguard.interfaces = {
-    #   wg0 = {
-    #     ips = [ "192.168.7.3/24" ];
-    #     listenPort = 51821;
-    #     privateKeyFile = "${config.sops.secrets.x1-wireguard-private-key.path}";
-    #     peers = [
-    #       {
-    #         # home-lab
-    #         publicKey = "Ug1P6UzLyQ0BFbrfi0G9KLJBNBs+IOisRn3uiLoR5yU=";
-    #         allowedIPs = [ "192.168.7.1" ];
-    #         endpoint = "vpn.fedeizzo.dev:51821";
-    #         # dynamicEndpointRefreshSeconds = 5;
-    #         persistentKeepalive = 25;
-    #       }
-    #     ];
-    #   };
-    # };
+    interfaces.wlp0s20f3 = { }; # define the interface for the topology network generation
+    wireguard.enable = true;
+    wireguard.interfaces = {
+      wg0 = {
+        ips = [ "192.168.7.3/24" ];
+        listenPort = 51821;
+        privateKeyFile = "${config.sops.secrets.x1-wireguard-private-key.path}";
+        peers = [
+          {
+            # home-lab
+            publicKey = "Ug1P6UzLyQ0BFbrfi0G9KLJBNBs+IOisRn3uiLoR5yU=";
+            allowedIPs = [ "192.168.7.1" ];
+            endpoint = "vpn.fedeizzo.dev:51821";
+            # dynamicEndpointRefreshSeconds = 5;
+            persistentKeepalive = 25;
+          }
+        ];
+      };
+    };
   };
 }
