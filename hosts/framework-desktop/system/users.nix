e9184226ea9c0b1dcdb1dabb73ef31399cadaa16
Federico Izzo <federico@fedeizzo.dev>
2026-03-01 12:50:57 +0100

    feat(homelab): improve homelab shell

--- hosts/framework-desktop/system/users.nix
+++ hosts/framework-desktop/system/users.nix
@@ -20,6 +20,10 @@
     dnsmasq
     hostapd
     htop
+    fzf
+    bat
+    fd
+    eza
     bottom
 
     # framework desktop specific
@@ -30,10 +34,40 @@
     rocmPackages.rocminfo
     btop-rocm
   ];
-
-  programs.bash = {
-    completion.enable = true;
-    enableLsColors = true;
+  programs = {
+    bash = {
+      completion.enable = true;
+      enableLsColors = true;
+      shellAliases = {
+        ls = "eza --icons --sort=type";
+        ll = "eza -l --icons --sort=type";
+        lll = "eza -l --icons --sort=type | less";
+        lla = "eza -la --icons --sort=type";
+        llt = "eza -T --icons --sort=type";
+        cat = "bat";
+        find = "fd";
+      };
+    };
+    starship = {
+      enable = true;
+      interactiveOnly = true;
+      settings = {
+        add_newline = false;
+        character = {
+          success_symbol = "[λ](green)";
+          vicmd_symbol = "[V](green)";
+          error_symbol = "[✖](red)";
+        };
+        package.disabled = true;
+        directory.truncation_length = 8;
+        cmd_duration = {
+          min_time = 20000;
+          format = "  [$duration](bold yellow)";
+        };
+      };
+    };
+    fzf.fuzzyCompletion = true;
+    fzf.keybindings = true;
   };
 
   environment = {
