10c7d6cdf9019aac991b232b23f580149486c25a
Federico Izzo <federico@fedeizzo.dev>
2026-06-08 19:23:17 +0200

    refactor: remove xps homelab

--- hosts/xps-9510-homelab/services/opencloud/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  users.users.opencloud.uid = 997; # make backup consistent across machines
-  services.opencloud = {
-    enable = false;
-    stateDir = "/var/lib/opencloud";
-  };
-}
